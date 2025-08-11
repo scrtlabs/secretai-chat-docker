cb0079906c9deae9caa74e3d542c6af08ce06fc6
DeDTihoN <tihonmarkp@gmail.com>
2025-08-11 14:57:13 +0300

    updated requirements

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ async def websocket_chat(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
